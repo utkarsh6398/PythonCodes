c0cdbe8e9fde4ee655691929cf69344bcd7b46b3
Kumar Utkarsh <u.kumar@holosuit.com>
2023-03-27 23:12:36 +0530

    SumList.py code modified and completed

--- SumList.py
+++ SumList.py
@@ -10,18 +10,20 @@ sll.getVal(s2)
 sll.showVal(s1)
 sll.showVal(s2)
 
+#Head pointers for the two LL
 t1=s1.head
 t2=s2.head
 digit1,digit2=0,0
 carry=0
-tmp=s3.head
-while t1.next!=None or t2.next!=None:
+
+#Driver Code
+while t1!=None or t2!=None:
     
-    if t1.next==None:
+    if t1==None:
+        digit1=0
         digit2=int(t2.val)
         t2=t2.next
-        sum=digit1+digit2+carry
-    elif t2.next==None:
+    elif t2==None:
         digit2=0
         digit1=int(t1.val)
         t1=t1.next
@@ -32,29 +34,25 @@ while t1.next!=None or t2.next!=None:
         t2=t2.next
 
     #Addition code
-    # print(digit1,"+",digit2)
-
+    sum=digit1+digit2+carry
     carry=0
     if sum>9:
         sum=sum-10
         carry=1
+    
+    #Adding sum of each digit to a new node in reverse order
     n=sll.Node(sum)
-    print(digit1,"+",digit2,"=",sum)
-    if tmp==s3.head:
+    if s3.head==None:
         s3.head=n
         s3.tail=n
-        tmp=s3.head
     else:
-        tmp.next=n
-        s3.tail=n
-        tmp=s3.tail
-
+        n.next=s3.head
+        s3.head=n
 
 
 if carry==1:
     n=sll.Node(carry)
-    tmp.next=n
-    s3.tail=n
-    tmp=s3.tail
+    n.next=s3.head
+    s3.head=n
 
 sll.showVal(s3)
