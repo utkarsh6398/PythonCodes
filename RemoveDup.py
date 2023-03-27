69dc05d25e3d441b48933a609e204489bed13480
Kumar Utkarsh <u.kumar@holosuit.com>
2023-03-27 13:46:03 +0530

    Remove Duplicate from SLL created

--- RemoveDup.py
+++ RemoveDup.py
@@ -1,21 +1,24 @@
+#Remove Dup from SLL
 import SLL_Class as sll
 
+#Linked List created and values are fetched
 s=sll.SinglyLinkList()
-
-
-# tmp=None
-# nodes=int(input("Enter no of Nodes"))
-# for i in range(0,nodes):
-#     val=int(input("Enter the value of the node"))
-#     n=sll.Node(val)
-#     if s.head==None:
-#         s.head=n
-#         tmp=s.head
-#     else:
-#         tmp.next=n
-#         tmp=tmp.next
-
-# s.tail=tmp
 sll.getVal(s)
 sll.showVal(s)
 
+#Empty list to hold values and check dup values
+l=[]
+
+tmp=s.head
+l.append(tmp.val)
+
+while tmp != s.tail:
+    if tmp.next.val in l:
+        tmp.next=tmp.next.next
+    else:
+        l.append(tmp.val)
+
+    tmp=tmp.next
+
+print("LL after removing dup nodes: ")
+sll.showVal(s)
